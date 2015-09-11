b346d754b2d073ac15392531f6a9b964a1e6bc39
v1jy4 <vijay.gopal.c@gmail.com>
2015-09-12 00:09:52 +0200

    auto typing of lambdas

--- lambda.cpp
+++ lambda.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <complex>
 
 int main()
 {
 	auto func = [](auto input){return input*input;};
-	std::cout<<func(10)<<std::endl;
+	std::cout<<func(10.12)<<std::endl;
+	std::cout<<func(std::complex<double>(2,-2))<<std::endl;
 	return 0;
 }
